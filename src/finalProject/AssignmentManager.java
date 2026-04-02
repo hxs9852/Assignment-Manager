0c057a20aaba40ef2159bddf19b9ae09374cb33b
Hayden1s <Hayden1sangosina@gmail.com>
2026-04-02 16:45:35 -0500

    Sart assignmnet manager

---
+++ src/finalProject/AssignmentManager.java
@@ -0,0 +1,38 @@
+package finalProject;
+import java.util.ArrayList;
+
+public class AssignmentManager {
+    //Instance Variables
+    private ArrayList<Assignment> assignments;
+
+    //Constructor
+    AssignmentManager(){
+        assignments = new ArrayList<>();
+    }
+
+    public void addAssignmnet(Assignment newAssignmnet){
+        assignments.add(newAssignmnet);
+    }
+
+    public void removeAssignmnet(Assignment toRemove){
+        assignments.remove(toRemove);
+    }
+
+    public void sortAssignmnets(){
+        assignments.sort(null);
+    }
+
+    public double getAverage(){
+        double average;
+        Assignment x;
+
+        for (int i = 0; i < assignments.size(); i++) {
+           x = assignments.get(i);
+           average += x.getGrade();
+        }
+
+        average = average / assignments.size();
+
+        return average;
+    }
+}
