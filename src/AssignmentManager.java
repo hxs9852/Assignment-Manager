0ee132ce2dd714ada7f3fcf3283bb17510c19699
Hayden1s <Hayden1sangosina@gmail.com>
2026-04-02 16:31:37 -0500

    Fix name

---
+++ src/AssignmentManager.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+
+public class AssignmentManager {
+    //Instance Variables
+    ArrayList<Assignment> assignments;
+
+    //Constructor
+    AssignmentManager(){
+        assignments = new ArrayList<>();
+    }
+
+}
