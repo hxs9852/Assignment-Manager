0d5df419529067bcc0e7e5a4af273d99ab198b64
Hayden1s <Hayden1sangosina@gmail.com>
2026-04-07 16:25:28 -0500

    Test class

--- src/finalProject/Test.java
+++ src/finalProject/Test.java
@@ -1,5 +1,34 @@
 package finalProject;
 
 public class Test extends Assignment{
+    //Instance Variable
+    private int numOfQuestions;
 
+    //Constructor
+    Test(){
+        super();
+        this.numOfQuestions = 1;
+    }
+    Test(double grade, Date due, String title, String course, int questions){
+        super(grade, due, title, course);
+        this.numOfQuestions = questions;
+    }
+
+    //Setters
+    public void setNumOfQuestions(int questions){
+        this.numOfQuestions = questions;
+    }
+    //Getters
+    public int getNumOfQuestions(){
+        return this.numOfQuestions;
+    }
+
+    //Methods
+    public double calculateGrade(int numberCorrect){
+        return (double) (numberCorrect / getNumOfQuestions()) * 100;
+    }
+
+    public String toString() {
+        return "[Test]: " + super.toString() + "\n" + "Number of Questions: " + getNumOfQuestions();
+    }
 }
