0a7c58f2e557846f955e11fa080e73bb25f083d7
carolinasweet <cornbreadboy18@gmail.com>
2026-04-09 16:41:45 -0500

    more gui work (not done)

--- src/finalProject/AssignmentManagerFE.java
+++ src/finalProject/AssignmentManagerFE.java
@@ -6,7 +6,7 @@ import javax.swing.border.*;
 public class AssignmentManagerFE {
 	private static AssignmentManager backend;
 	
-	private static JTextField addTitle, addCourse, addDay, addMonth, addYear, addGrade, addGrade2;
+	private static JTextField addTitleField, addCourseField, addDayField, addMonthField, addYearField, addGradeField, addGrade2Field;
 	private static JTextField manageTitle;
 	private static JComboBox<SortType> addType, displaySort;
 	private static JTextArea output;
@@ -16,10 +16,37 @@ public class AssignmentManagerFE {
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setLayout(null);
 		
+		////ADD PANEL STUFF
+		
 		JPanel addPanel = new JPanel(null);
-		addPanel.setBounds(10, 10, 100, 100);
+		addPanel.setBounds(10, 10, 200, 200);
 		addPanel.setBorder(new TitledBorder("Add"));
 		frame.add(addPanel);
+		
+		//add title
+		JLabel addTitleLabel = new JLabel("Title:");
+		addTitleLabel.setBounds(20,20,50,30);
+		addPanel.add(addTitleLabel);
+		
+		addTitleField = new JTextField();
+		addTitleField.setBounds(70,20,120,30);
+		addPanel.add(addTitleField);
+		
+		//add course
+		JLabel addCourseLabel = new JLabel("Course:");
+		addCourseLabel.setBounds(20,50,50,30);
+		addPanel.add(addCourseLabel);
+		
+		addCourseField = new JTextField();
+		addCourseField.setBounds(70,50,120,30);
+		addPanel.add(addCourseField);
+		
+		//add course
+		JLabel addDateLabel = new JLabel("Due Date:   -  -  ");
+		addDateLabel.setBounds(20,80,70,30);
+		addPanel.add(addDateLabel);
+		
+		//i want to replace this with a jcombobox of months and integers and stuff
 		
 		frame.setVisible(true);
 	}
