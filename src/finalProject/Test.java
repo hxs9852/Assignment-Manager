9d89f175551bd3f0f030cd2f6f80f763c323ae26
carolinasweet <cornbreadboy18@gmail.com>
2026-04-19 14:41:54 -0500

    grade calculation fixed AGAIN

--- src/finalProject/Test.java
+++ src/finalProject/Test.java
@@ -3,7 +3,6 @@ package finalProject;
 public class Test extends Assignment{
     //Instance Variable
     private int numOfQuestions;
-    private double gradePercent; //overriding this one a tad
 
     //Constructor
     Test(){
@@ -13,22 +12,25 @@ public class Test extends Assignment{
     Test(Date due, String title, String course, int correct, int questions){
         super(0, due, title, course);
         this.numOfQuestions = questions;
-        this.calculateGrade(correct);
+        this.setGrade(correct);
     }
 
     //Setters
     public void setNumOfQuestions(int questions){
         this.numOfQuestions = questions;
     }
+    
+    public void setGrade(int numberCorrect){
+        super.setGrade((double) (numberCorrect / getNumOfQuestions()) * 100);
+    }
+    
     //Getters
     public int getNumOfQuestions(){
         return this.numOfQuestions;
     }
 
     //Methods
-    public void calculateGrade(int numberCorrect){
-        this.gradePercent = (double) (numberCorrect / getNumOfQuestions()) * 100;
-    }
+    
 
     public String toString() {
         return "[Test]: " + super.toString() + "\n" + "Number of Questions: " + getNumOfQuestions();
